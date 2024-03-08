e4f1c65b42131ca5a16efc2a79b6a711e5499ee9
wpiron10 <w.piron.perso@gmail.com>
2024-03-08 12:06:24 +0100

    William : ADD hero banner & MOD compressed svg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,14 @@
 @endsection
 
 @section('content')
-<section>
-    <div style="min-height: 100vh;">
-        <h1>Home</h1>
+<section class="hero_section">
+    <img src="" alt="" class="hero_img" />
+    <div class="hero_content">
+        <p>100% Natural Food</p>
+        <h1>Choose the best
+            healthier way
+            of life</h1>
+        <button>Explore Now</button>
     </div>
 </section>
 @endsection
